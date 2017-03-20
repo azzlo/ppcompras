21f67aea1dc61d0261284a3d072d0af5aa74a7a7
Oscar Tellez <oscartzgz@gmail.com>
2017-03-20 17:43:57 -0600

    Agregue el hola

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170320232608) do
 
   create_table "offers", force: :cascade do |t|
@@ -24,7 +23,6 @@ ActiveRecord::Schema.define(version: 20170320232608) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.index ["requisition_id"], name: "index_offers_on_requisition_id"
-
   end
 
   create_table "requisitions", force: :cascade do |t|
@@ -39,7 +37,6 @@ ActiveRecord::Schema.define(version: 20170320232608) do
     t.text     "observations"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-
   end
 
   create_table "suppliers", force: :cascade do |t|
