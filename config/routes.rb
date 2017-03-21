8b93143d46b3fd9b48c7b1e59a1da38335ba4723
Oscar Tellez <oscartzgz@gmail.com>
2017-03-21 17:20:35 -0600

    Algunos estulos para el topbar y accion de logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
+  root 'welcome#index'
+  devise_for :users
+  resources :users
   resources :offers
   resources :requisitions
   resources :suppliers
-  devise_for :users
-  resources :users
-  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
